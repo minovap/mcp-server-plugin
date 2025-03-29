2bc11eec4ec12cdf7f0ded2b2c6f73d4933ebb1f
tomat <tomat@blarg.se>
2025-03-29 11:42:20 +0100

    always un-exclude node_modules inside the project dir

--- src/main/kotlin/org/jetbrains/mcpserverplugin/utils/filesearch/IsFileIncludedInProject.kt
+++ src/main/kotlin/org/jetbrains/mcpserverplugin/utils/filesearch/IsFileIncludedInProject.kt
@@ -9,6 +9,14 @@ import com.intellij.openapi.vfs.VirtualFile
  * Check if a file is included in the project (not excluded).
  */
 internal fun FileSearch.isFileIncludedInProject(file: VirtualFile, project: Project): Boolean {
+    // Always return true for node_modules directory inside the project directory
+    val filePath = file.path.replace("\\", "/")
+    val projectBasePath = project.basePath?.replace("\\", "/")
+    
+    if (projectBasePath != null && filePath.startsWith(projectBasePath) && filePath.contains("/node_modules")) {
+        return true
+    }
+    
     return runReadAction {
         val fileIndex = ProjectRootManager.getInstance(project).fileIndex
         fileIndex.isInContent(file)
